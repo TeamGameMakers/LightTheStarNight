9280fd81cfc5f932cca1cddce2fd048af94d46df
Arno Clare <ArnoClareF@outlook.com>
2022-08-07 21:58:30 +0800

    完成游戏通关 Panel.

--- Assets/Scripts/UI/SettingPanel.cs
+++ Assets/Scripts/UI/SettingPanel.cs
@@ -2,6 +2,18 @@
 {
     public class SettingPanel : BasePanel
     {
-        
+        protected override void OnClick(string btnName)
+        {
+            base.OnClick(btnName);
+            switch (btnName)
+            {
+                case "BackBtn":
+                    break;
+                case "KeyBtn":
+                    break;
+                case "BackStartBtn":
+                    break;
+            }
+        }
     }
 }
