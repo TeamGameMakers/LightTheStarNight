61a1eea167eb687d118735e40909abb2b57e6c09
Arno Clare <ArnoClareF@outlook.com>
2022-08-07 23:55:34 +0800

    完善星灯的视觉和音效表现。

--- Assets/Scripts/Interact/Object/StarLamp.cs
+++ Assets/Scripts/Interact/Object/StarLamp.cs
@@ -1,7 +1,9 @@
 using System;
 using Base;
+using Base.Resource;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 namespace Interact
 {
@@ -19,16 +21,87 @@ namespace Interact
         private Animator m_animator;
         private static readonly int Brighten = Animator.StringToHash("Brighten");
 
+        private AudioSource m_audioSource;
+
+        private bool m_readyPlayLightClip = false;
+
+        public new Light2D light;
+
         private void Awake()
         {
             m_animator = GetComponent<Animator>();
+            m_audioSource = GetComponent<AudioSource>();
+            if (m_audioSource == null)
+            {
+                m_audioSource = gameObject.AddComponent<AudioSource>();
+                m_audioSource.playOnAwake = false;
+                m_audioSource.loop = false;
+            }
+        }
+
+        private void Update()
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                ++m_lighten;
+                // 触发音效
+                switch (m_lighten)
+                {
+                    case 1:
+                        m_audioSource.clip = ResourceLoader.Load<AudioClip>("Audio/starlight1");
+                        m_audioSource.Play();
+                        break;
+                    case 2:
+                        m_audioSource.clip = ResourceLoader.Load<AudioClip>("Audio/starlight2");
+                        m_audioSource.Play();
+                        break;
+                    case 3:
+                        m_audioSource.clip = ResourceLoader.Load<AudioClip>("Audio/starlight3");
+                        m_audioSource.Play();
+                        m_readyPlayLightClip = true;
+                        break;
+                }
+                // 改变自身显示状态
+                m_animator.SetTrigger(Brighten);
+                
+                // 完全亮起，触发点亮事件
+                if (m_lighten == needLighten)
+                {
+                    EventCenter.Instance.EventTrigger(LightenFullEvent);
+                }
+            }
+            // 检查播放最终音效
+            if (m_readyPlayLightClip && m_audioSource.time >= 1.3)
+            {
+                m_audioSource.clip = ResourceLoader.Load<AudioClip>("Audio/starlightACT");
+                m_audioSource.Play();
+                m_readyPlayLightClip = false;
+                light.gameObject.SetActive(true);
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D col)
         {
-            if (col.CompareTag("StarLight"))
+            if (col.CompareTag("StarLight") && m_lighten < needLighten)
             {
                 ++m_lighten;
+                // 触发音效
+                switch (m_lighten)
+                {
+                    case 1:
+                        m_audioSource.clip = ResourceLoader.Load<AudioClip>("Audio/starlight1");
+                        m_audioSource.Play();
+                        break;
+                    case 2:
+                        m_audioSource.clip = ResourceLoader.Load<AudioClip>("Audio/starlight2");
+                        m_audioSource.Play();
+                        break;
+                    case 3:
+                        m_audioSource.clip = ResourceLoader.Load<AudioClip>("Audio/starlight3");
+                        m_audioSource.Play();
+                        m_readyPlayLightClip = true;
+                        break;
+                }
                 // 改变自身显示状态
                 m_animator.SetTrigger(Brighten);
                 // 销毁
