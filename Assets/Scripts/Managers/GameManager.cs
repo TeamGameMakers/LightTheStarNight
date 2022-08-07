2e1a295296bd0096eb8940a4492204f78d33e826
Arno Clare <ArnoClareF@outlook.com>
2022-08-07 12:19:27 +0800

    完成 GameManager 的基础结构。

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GM
@@ -7,13 +8,7 @@ namespace GM
     {
         // 游玩
         Playing,
-        // 剧情
-        Story,
-        // 解谜
-        Puzzle,
-        // 推针
-        PinLock,
-        CG
+        UI
     }
     
     public static class GameManager
@@ -21,5 +16,46 @@ namespace GM
         public static Transform Player { get; private set; }
         
         public static void SetPlayerTransform(Transform player) => Player = player;
+
+        private static GameState m_currentState = GameState.Playing;
+        
+        private static Stack<GameState> m_stateRecord = new Stack<GameState>();
+
+        public static event Action<GameState> SwitchStateEvent;
+
+        public static void ResumeState()
+        {
+            m_currentState = m_stateRecord.Pop();
+        }
+
+        public static void ClearState(GameState toState = GameState.Playing)
+        {
+            m_stateRecord.Clear();
+            m_currentState = toState;
+        }
+        
+        public static void SwitchState(GameState state, bool recordState = true)
+        {
+            if (recordState)
+                m_stateRecord.Push(m_currentState);
+            m_currentState = state;
+
+            switch (state)
+            {
+                case GameState.Playing:
+                    InputHandler.SwitchToPlayer();
+                    break;
+                case GameState.UI:
+                    InputHandler.SwitchToUI();
+                    break;
+            }
+            
+            SwitchStateEvent?.Invoke(state);
+        }
+
+        public static void GameOver()
+        {
+            // TODO: 弹出游戏结束面板
+        }
     }
 }
